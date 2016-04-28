547e98f68fbdf0c2558687026f0be7f5d6843233
Peter Yoachim <yoachim@uw.edu>
2016-04-28 10:10:05 -0700

    blah

--- fast_hp/run_parallel.sh
+++ fast_hp/run_parallel.sh
@@ -1,5 +1,7 @@
 !#/bin/bash
 
 # to run:
-# nohup ./run_parallel.sh 
-cat run_many_months.txt | xargs -I '{}' -n 1 -P 3 run_month.sh '{}'
+# source '/home/yoachim/anaconda2/bin/eups-setups.sh'
+# setup sims_skybrightness
+# nohup ./run_parallel.sh &
+cat run_many_months.txt | xargs -I '{}' -n 1 -P 3 sh -c '{}'
